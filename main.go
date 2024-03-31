96184da9528702be9e811b2a9038b49639eda11e
Antero Montalvo <antero2203@gmail.com>
2024-03-31 15:10:14 -0500

    Siguiente commit

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 
+	"github.com/antero/go/ejercicios"
 	"github.com/antero/go/variables"
 )
 
@@ -10,5 +12,22 @@ func main(){
 	estado, texto := variables.ConviertoaTexto(1588)
 	fmt.Println(estado)
 	fmt.Println(texto)
+	
+	if os := runtime.GOOS; os == "linux" || os == "Os X."{
+		fmt.Println("Esto no es Windows", os)
+	} else {
+		fmt.Println("Esto es Windows")
+	}
 
+	switch os:= runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")	
+	default:
+		fmt.Printf("%s \n", os)
+	}
+	numero, texto := ejercicios.ConvNumerico("500")
+	fmt.Println(numero)
+	fmt.Println(texto)
 }
